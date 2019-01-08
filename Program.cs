3571dd9ed42f87dbfbbeddce2fd646c6918e3d67
Bob Tabor <bobtabor@outlook.com>
2019-01-08 11:05:37 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringInterpolationTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bob";
+            string date = "Tuesday";
+
+            // There's two parts to string interpolation:
+            // 1. Add a $ dollar sign before the literal string.
+            // 2. Use {} to merge a variable into the literal string.
+
+            Console.WriteLine($"Hello {name}, it's {date}!");
+
+            // While you can merge like this:
+            // string message = $"Hello {name}, it's {date}!";
+            // Console.WriteLine(message);
+
+            // You can't do this:
+            // string message = "Hello {name}, it's {date}!";
+            // Console.WriteLine($message);
+        }
+    }
+}
